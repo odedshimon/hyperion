6fa2c60f4a75e2d4b9071982820ad3107e9fdaaf
odedshimon <port606060>
2021-11-17 00:40:10 +0200

    get domain value from text box

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -5,30 +5,33 @@
         <title>Hyperion OSINT System</title>
     </head>
     <h3>Enter domain name</h3>
-    <input type="text" name="main-text-box">
-      <button @click="fetchData">Fetch</button>
+    <input type="text" name="domain-text-box" id="domain-text-box">
+    <button @click="fetchData">Fetch</button>
+    <label>{{ whoisData }}</label>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 
-const backendURL = "http://localhost:5000/whois?domain=ynet.com"
+const backendURL = "http://localhost:5000/whois?domain="
 
 export default {
   props: {
   },
   data(){
     return{
-      whoisData: null
+      whoisData: [],
+      domain: ""
     }
   },
   methods: {
-    fetchData: function(){
-      console.log("before")
-      axios.get(backendURL).then(response => (this.whoisData = response))
-      console.log("after")
-      console.log(this.whoisData)
+    fetchData: async function(){
+      this.domain = document.getElementById('domain-text-box').value
+      console.log("Sending request to backend")
+      const response = await axios.get(backendURL + this.domain)
+      this.whoisData = response.data
+      console.log("Response accepted from backend")
     }
   }
 }
